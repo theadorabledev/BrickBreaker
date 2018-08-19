d5e7f392f0baaa880ef73a4460261f613ac01b2b
Theadora Williams <theadorabledeveloper@gmail.com>
2018-08-19 16:16:46 -0400

    Changed from three characters per coordinate to one.

--- BrickBreaker.py
+++ BrickBreaker.py
@@ -1,38 +1,76 @@
 from Tkinter import *
 import time
 HEIGHT = 11
-WIDTH = 21
+WIDTH = 64
 BRICKHEIGHT = 2
+PLATFORMLENGTH = 3
 class Application(Frame): 
     
     def moveBall(self):
+        self.grid[self.ballPos[1]][self.ballPos[0]] = "O"
         newX = self.ballPos[0] + self.ballDirection[0]
         newY = self.ballPos[1] + self.ballDirection[1]
-        if newX >= HEIGHT or newX < 0: 
+        print newY, HEIGHT
+        if newX >= WIDTH or newX <= 0: 
             self.ballDirection[0] *= -1 
-        if newY >= WIDTH or newY < 0: 
-            self.ballDirection[1] *= -1         
-    
-        self.grid[self.ballPos[0]][self.ballPos[1]] = "   "
-        print self.ballPos
-        self.ballPos[0] = self.ballPos[0] + self.ballDirection[0]
-        self.ballPos[1] = self.ballPos[1] + self.ballDirection[1]
-        print self.ballPos
-        print "x"
-        self.grid[self.ballPos[0]][self.ballPos[1]] = " O "
-        self.createWidgets()            
-
+        if newY <= 0: 
+            self.ballDirection[1] *= -1       
+        try:
+            if newY  >= (HEIGHT):
+             #   self.game["bg"] = "red"
+                self.grid[self.ballPos[1]][self.ballPos[0]] = " "
+                self.ballPos = self.origBallPos[:]
+                self.grid[self.ballPos[1]][self.ballPos[0]] = "O"
+        except IndexError:
+            pass
+        else:
+            try:
+                if self.grid[newY][newX] == "-":
+                    self.ballDirection[1] *= -1 
+                if self.grid[newY][newX] == "[":
+                    self.ballDirection[1] *= -1 
+                    self.grid[newY][newX] = " "
+                    self.grid[newY][newX + 1] = " "
+                    self.grid[newY][newX + 2] = " "
+                    self.playerPoints += 1
+                if self.grid[newY][newX] == "_":
+                    self.ballDirection[1] *= -1 
+                    self.grid[newY][newX - 1] = " "
+                    self.grid[newY][newX] = " "
+                    self.grid[newY][newX + 1] = " "                    
+                if self.grid[newY][newX] == "]":
+                    self.ballDirection[1] *= -1    
+                    self.grid[newY][newX - 2] = " "
+                    self.grid[newY][newX - 1] = " "
+                    self.grid[newY][newX] = " "                    
+            except IndexError:
+                pass
+            self.grid[self.ballPos[1]][self.ballPos[0]] = " "
+           # print "orig", str(self.ballPos)
+            #print self.platformPos[0]
+            self.ballPos[0] = self.ballPos[0] + self.ballDirection[0]
+            self.ballPos[1] = self.ballPos[1] + self.ballDirection[1]
+            print "new", str(self.ballPos)
+            print "x"
+            self.grid[self.ballPos[1]][self.ballPos[0]] = "O"
+            self.createWidgets()            
         self.master.after(1000, self.moveBall)
+    def changeBallDirection(self):
+        pass
     def movePlatform(self,event):
         move = 1
         if event.keysym == "Left":
             move = -1
 
-        if self.selectedPos + move < 0 or self.selectedPos + move > 20:
+        if self.platformPos[0] - 1 + move < 0 or self.platformPos[0] + 1 + move > WIDTH:
             return False            
-        self.grid[len(self.grid)-1][self.selectedPos] = "   "
-        self.selectedPos += move
-        self.grid[len(self.grid)-1][self.selectedPos] = "---"
+        self.grid[self.platformPos[1]][self.platformPos[0] - 1] = " "
+        self.grid[self.platformPos[1]][self.platformPos[0]] = " "
+        self.grid[self.platformPos[1]][self.platformPos[0] + 1] = " "
+        self.platformPos[0] += move
+        self.grid[self.platformPos[1]][self.platformPos[0] - 1] = "-"
+        self.grid[self.platformPos[1]][self.platformPos[0]] = "-"
+        self.grid[self.platformPos[1]][self.platformPos[0] + 1] = "-"
         self.createWidgets()
     def updateGridAsText(self):
         self.gridAsText=""
@@ -42,24 +80,29 @@ class Application(Frame):
         self.updateGridAsText()
         self.game = Label(self, text=self.gridAsText, font='TkFixedFont', borderwidth=4, relief="groove")
         self.game.grid(row=1, column=1)
-    def swapShowMoves(self):
-        self.showMoves = not self.showMoves
+
     def __init__(self, master=None):
         Frame.__init__(self, master)
         self.pack()
-        bricks =  [["[_]"] * WIDTH for i in  range(BRICKHEIGHT)]
-        space = [["   "] * WIDTH for i in  range(HEIGHT-BRICKHEIGHT-1)]
-        platform = [(["   "] * (WIDTH/2)) + ["---"] + (["   "] * (WIDTH/2))]
-        self.grid = bricks + space + platform 
-        self.grid[HEIGHT/2] = (["   "] * (WIDTH/2)) + [" O "] + (["   "] * (WIDTH/2))
+        bricks =  [["[","_","]"] * (WIDTH/3) for i in  range(BRICKHEIGHT)]
+        space = [[" "] * WIDTH for i in  range(HEIGHT-BRICKHEIGHT-2)]
+        platform = [([" "] * (WIDTH/2)) + ["-"] * PLATFORMLENGTH + ([" "] * (WIDTH/2))]
+        abyss = [[" "] * WIDTH ]
+        self.grid = bricks + space + platform + abyss
+        self.grid[HEIGHT/2] = ([" "] * (WIDTH/2)) + ["O"] + ([" "] * (WIDTH/2))
+        
         self.bind("<Left>", self.movePlatform)
         self.bind("<Right>", self.movePlatform)
         self.focus_set()
         self.gridAsText=""
         self.createWidgets()
-        self.selectedPos = 10
-        self.ballPos = [HEIGHT/2, WIDTH/2]
+        self.platformPos = [(WIDTH/2) + 1, len(self.grid)-2]
+        self.ballPos = [WIDTH/2, HEIGHT/2]
+        self.playerLives = 3
+        self.playerPoints = 0
+        self.origBallPos = [WIDTH/2, HEIGHT/2]
         self.ballDirection = [1, 1]
+        self.ballSlope = [1,1]#run, rise
         self.moveBall()
         
 
